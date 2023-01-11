5a0bbb170d0004bfde752481cd2a20a3bbc54582
AndrinMagRinder <Andrin.kaech@gmail.com>
2023-01-11 16:37:35 +0100

    Add files via upload

---
+++ Auto-Simulator_form/Auto-Simulator_form/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Simulator_form
+{
+    public partial class Form1 : Form
+    {
+        Auto auto = null;
+     //   Lastwagen lastwagen = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbx_Fahrzeug.DisplayMember = "CarName";
+            lbl_status.Text = "ON";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbx_Fahrzeug.Items.Add(new Auto("VW Golf", 200));
+            cbx_Fahrzeug.Items.Add(new Auto("BMW M4", 430));
+            cbx_Fahrzeug.Items.Add(new Auto("Lamborghini Huracan", 640));
+            cbx_Fahrzeug.Items.Add(new Lastwagen("Laster", 330));
+
+        }
+
+        private void cbx_Cars_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            auto = cbx_Fahrzeug.SelectedItem as Auto;
+       //     lastwagen = cbx_Fahrzeug.SelectedItem as Lastwagen;
+       //     lbl_PS.Text = lastwagen.TruckHP.ToString();
+       //     cbx_Fahrzeug.Text = lastwagen.TruckName.ToString();
+
+            lbl_PS.Text = auto.CarHP.ToString();
+            cbx_Fahrzeug.Text = auto.CarName.ToString();
+            if (auto.Status == 1)
+            {
+                lbl_status.Text = "OFF";
+            }
+            if (auto.Status == 0)
+            {
+                lbl_status.Text = "ON";            }
+
+            lbl_ShowSpeed.Text = auto.Speed.ToString();
+            lbl_ShowGear.Text = auto.Gear.ToString();
+        }
+
+        private void lbl_status_Click(object sender, EventArgs e)
+        {
+            if (this.auto != null)
+            {
+                if (lbl_status.Text == "ON")
+                {
+                    lbl_status.Text = "OFF";
+                    auto.StartEngine();
+                    lbl_ShowSpeed.Text = auto.Speed.ToString();
+                    lbl_ShowGear.Text = auto.Gear.ToString();
+                    auto.EngineSound();
+                }
+                else
+                {
+                    lbl_status.Text = "ON";
+                    auto.StopEngine();
+                    lbl_ShowSpeed.Text = auto.Speed.ToString();
+                    lbl_ShowGear.Text = auto.Gear.ToString();
+                }
+            }
+        }
+
+        private void btn_Brake_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void btn_Gas_Hover(object sender, EventArgs e)
+        {
+            if (auto.Status == 1)
+            {
+                auto.Accelerate();
+            }
+        }*/
+
+        private void btn_Gas_MouseHover_1(object sender, EventArgs e)
+        {
+            if (this.auto != null)
+            {
+                auto.Accelerate();
+                if (auto.Status == 1)
+                {
+                    auto.SetGear();
+                }
+                lbl_ShowSpeed.Text = auto.Speed.ToString();
+                btn_Gas.Visible = false;
+                btn_Gas.Visible = true;
+                lbl_ShowGear.Text = auto.Gear.ToString();
+            }
+        }
+
+        private void btn_Brake_MouseHover(object sender, EventArgs e)
+        {
+            if (this.auto != null)
+            {
+                auto.Brake();
+                if (auto.Status == 1)
+                {
+                    auto.SetGear();
+                }
+                lbl_ShowSpeed.Text = auto.Speed.ToString();
+                btn_Brake.Visible = false;
+                btn_Brake.Visible = true;
+                lbl_ShowGear.Text = auto.Gear.ToString();
+            }
+        }
+
+        private void btn_honk_Click(object sender, EventArgs e)
+        {
+            if (auto != null && auto.Status == 1)
+            {
+                auto.Honk();
+            }
+        }
+
+    }
+}
---
+++ Auto-Simulator_form/Auto-Simulator_form/Lastwagen.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Simulator_form
+{
+    internal class Lastwagen : IFahrezeug
+    {
+        string truckName;
+        int truckHP;
+        int speed;
+        int status;
+        int gear;
+
+        public Lastwagen(string TruckName, int truckHP)
+        {
+            this.truckName = TruckName;
+            this.truckHP = truckHP;
+        }
+
+        public string TruckName
+        {
+            get { return truckName; }
+            set { truckName = value; }
+        }
+
+        public int TruckHP
+        {
+            get { return truckHP; }
+            set { truckHP = value; }
+        }
+
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public int Gear
+        {
+            get { return gear; }
+            set { gear = value; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+
+        public void Accelerate()
+        {
+            if (status == 1)
+            {
+                if (speed < ((TruckHP / 3) + 100))
+                {
+                    speed += (truckHP / 200);
+                }
+            }
+        }
+
+        public void Brake()
+        {
+            if (status == 1)
+            {
+                if (speed > 0)
+                {
+                    speed -= (truckHP / 100);
+                    if (speed < 0)
+                    {
+                        speed = 0;
+                    }
+                }
+            }
+        }
+
+        public void StartEngine()
+        {
+            status = 1;
+            gear = 1;
+        }
+
+
+        public void StopEngine()
+        {
+            status = 0;
+            gear = 0;
+            speed = 0;
+        }
+
+        public void SetGear()
+        {
+
+            if (speed >= 0)
+            {
+                gear = 1;
+            }
+
+            if (speed > (((TruckHP / 3) + 100) / 7 * 1))
+            {
+                gear = 2;
+            }
+
+            if (speed > (((TruckHP / 3) + 100) / 7 * 2))
+            {
+                gear = 3;
+            }
+
+            if (speed > (((TruckHP / 3) + 100) / 7 * 3))
+            {
+                gear = 4;
+            }
+
+            if (speed > (((TruckHP / 3) + 100) / 7 * 4))
+            {
+                gear = 5;
+            }
+
+            if (speed > (((TruckHP / 3) + 100) / 7 * 5))
+            {
+                gear = 6;
+            }
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine(System.IO.Path.GetFullPath("../../../honk.wav"));
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(System.IO.Path.GetFullPath("../../../honk.wav"));
+            player.Play();
+        }
+
+        public void EngineSound()
+        {
+            Console.WriteLine(System.IO.Path.GetFullPath("../../../engine.wav"));
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(System.IO.Path.GetFullPath("../../../engine.wav"));
+            player.Play();
+        }
+    }
+}
